421ba6c846566e764a2962c0fb405b3c87ea0d77
Rogach <platon7pronko@gmail.com>
2012-03-10 00:08:38 +0400

    init

---
+++ publish-to-repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name=`sed -n 's_\s*name\s*:=\s*"\(.*\)"_\1_p' build.sbt`
+version=`sed -n 's_\s*version\s*:=\s*"\(.*\)"_\1_p' build.sbt`
+scalaVersion=`sed -n 's_\s*scalaVersion\s*:=\s*"\(.*\)"_\1_p' build.sbt`
+sbt publish-local
+mkdir -p /home/platon/sync/org.rogach/org/rogach/${name}/${version}/
+cp ~/.ivy2/local/default/${name}_${scalaVersion}/${version}/jars/${name}_${scalaVersion}.jar \
+  /home/platon/sync/org.rogach/org/rogach/${name}/${version}/${name}-${version}.jar
+cat ~/.ivy2/local/default/${name}_${scalaVersion}/${version}/poms/${name}_${scalaVersion}.pom \
+  | sed 's/<groupId>default<\/groupId>/<groupId>org.rogach<\/groupId>/' \
+  | sed "s/<artifactId>${name}_${scalaVersion}<\/artifactId>/<artifactId>${name}<\/artifactId>/" \
+  > /home/platon/sync/org.rogach/org/rogach/${name}/${version}/${name}-${version}.pom
