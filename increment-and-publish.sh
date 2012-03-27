904a8230570d49ad92c42efbaf7c92a719350b24
Rogach <platon7pronko@gmail.com>
2012-03-27 17:22:24 +0400

    updated repository

--- increment-and-publish.sh
+++ increment-and-publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# increments last number of project version in build.sbt
+# increments last number of project version in build.sbt and readme
 perl -pe '$_=~s/\s*version\s*:=\s*"([0-9\.]*\.)(\d*)"/"version := \"" . $1 . ($2 + 1) . "\""/e;' < build.sbt | sponge build.sbt
+perl -pe '$_=~s/%\s"([0-9\.]*\.)(\d*)"/"% " . "\"" . $1 . ($2 + 1) . "\""/e;' < README.md | sponge README.md
 publish-to-repo.sh
